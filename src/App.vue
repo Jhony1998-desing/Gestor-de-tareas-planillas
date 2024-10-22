36c60c1155cf2234051dec10490302e679f7b163
Jhony benitez <jonathanbenitez_98@hotmail.com>
2024-10-22 13:00:23 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="my-4">Gestor de Tareas</h1>
+    <AgregarTarea @nueva-tarea="agregarTarea"></AgregarTarea>
+    <TareasDeAPI></TareasDeAPI>
+    <VistaCombinada :tareas="tareas"></VistaCombinada>
+  </div>
+</template>
+
+<script>
+import AgregarTarea from './components/AgregarTarea.vue';
+import TareasDeAPI from './components/TareasDeAPI.vue';
+import VistaCombinada from './components/VistaCombinada.vue';
+
+export default {
+  components: {
+    AgregarTarea,
+    TareasDeAPI,
+    VistaCombinada
+  },
+  data() {
+    return {
+      tareas: []
+    };
+  },
+  methods: {
+    agregarTarea(tarea) {
+      this.tareas.push(tarea);
+    }
+  }
+};
+</script>
